5265a9ebe35ec582735aa20bc0a4e2afb75a0ce0
crzyman77 <Chris@Gillis-MacBook-Pro.local>
2016-02-29 15:00:49 -0500

    Data Manipulation Queries
    Wrote the 2 queries Nate Needs for the event list and event details
    views.

--- model/model.php
+++ model/model.php
@@ -44,19 +44,54 @@
 		}		
 	}
         
+        function getEventDetails($eventid){
+            try{
+                $dataBase = getDBConnection();
+                $query = "SELECT \n"
+                        . "event.name, \n"
+                        . "event.start_time, \n"
+                        . "event.end_time, \n"
+                        . "event.event_date, \n"
+                        . "event.description, \n "
+                        . "venue.building_name, \n"
+                        . "venue.room_number \n"
+                        . "FROM \n"
+                        . "event INNER JOIN venue ON event.venue_id = venue.id  \n"
+                        . "WHERE event.id = :id";
+                $statement = $dataBase->prepare($query);
+                $statement->bindValue(':id', $eventid);
+                $statement->execute();
+                $results = $statement->fetchAll();
+                $statement->closeCursor();
+                return $results;           // Assoc Array of Rows
+                
+            } catch (PDOException $e) {
+			$errorMessage = $e->getMessage();
+                        echo $errorMessage;
+			include '../view/404.php';
+			die;
+
+            }
+        }
+        
         function getEventList(){
             try{
                 $dataBase = getDBConnection();
-                $query = "SELECT"
-                        . " event.name,"
-                        . "event.start_time,"
-                        . "event.end_time,"
-                        . "event.event_date,"
-                        . "event.organizer,"
-                        . "venue.building_name,"
-                        . "venue.room_number"
-                        . " FROM"
-                        . " event INNER JOIN venue ON event.venue_id = venue.id";
+                $query = "SELECT \n"
+    . " event.id, \n"
+    . " event.name, \n"
+    . " event.start_time, \n"
+    . " event.end_time, \n"
+    . " event.event_date, \n"
+    . " venue.building_name, \n"
+    . " venue.room_number \n"
+    . "FROM \n"
+    . " event \n"
+    . " INNER JOIN venue ON event.venue_id = venue.id \n"
+    . "WHERE \n"
+    . " event.event_date > CURDATE() \n"
+    . "ORDER BY \n"
+    . " event.event_date";
                 $statement = $dataBase->prepare($query);
                 $statement->execute();
                 $results = $statement->fetchAll();
@@ -91,27 +126,27 @@
             }
         }
         
-         function locationCheckStill(){
-            try{
-                $dataBase = getDBConnection();
-                $sql = "select venue.id,venue.building_name,venue.room_number,venue.corner1_lat,venue.corner1_lng,venue.corner2_lat,venue.corner2_lng,venue.corner3_lat,venue.corner3_lng,"
-    .                       "venue.corner4_lat,venue.corner4_lng FROM venue WHERE id = 1";      
-                $statement = $dataBase->prepare($sql);
-                $statement->execute();
-                $results = $statement->fetchAll();
-                $statement->closeCursor();
-                print_r($results);
-                return $results;
-            } catch (Exception $ex) {
-                $errorMessage = $ex->getMessage();
-                        echo $errorMessage;
-                        include '../view/404.php';
-                        die;
-
-
-
-        }
-        }
+//         function locationCheckStill(){
+//            try{
+//                $dataBase = getDBConnection();
+//                $sql = "select venue.id,venue.building_name,venue.room_number,venue.corner1_lat,venue.corner1_lng,venue.corner2_lat,venue.corner2_lng,venue.corner3_lat,venue.corner3_lng,"
+//    .                       "venue.corner4_lat,venue.corner4_lng FROM venue WHERE id = 1";      
+//                $statement = $dataBase->prepare($sql);
+//                $statement->execute();
+//                $results = $statement->fetchAll();
+//                $statement->closeCursor();
+//                print_r($results);
+//                return $results;
+//            } catch (Exception $ex) {
+//                $errorMessage = $ex->getMessage();
+//                        echo $errorMessage;
+//                        include '../view/404.php';
+//                        die;
+//
+//
+//
+//        }
+//        }
         
       
 
